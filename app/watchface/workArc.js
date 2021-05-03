0d7dab30499e4caaf6d121402e7d1dad75da681b
Alfie Woodland <alfiewoodland@gmail.com>
2021-05-03 21:16:51 +0100

    Cut off working hours at hour hand between days

--- app/watchface/workArc.js
+++ app/watchface/workArc.js
@@ -6,29 +6,54 @@ const workArcB = document.getElementById('workArcB');
 const workArcC = document.getElementById('workArcC');
 
 function setWorkArc(show, workingDays, startHour, endHour) {
-  
-  let currentDay = (new Date().getDay() + 6) % 7;
 
-  if (!show || workingDays.indexOf(currentDay) === -1) {
+  let startTime = new Date();
+  startTime.setHours(startHour, (startHour % 1) * 60, 0, 0);
+
+  let endTime = new Date();
+  endTime.setHours(endHour, (endHour % 1) * 60, 0, 0);
+
+  let now = new Date();
+  let currentDay = (now.getDay() + 6) % 7;
+
+  let workingToday = workingDays.indexOf(currentDay) > -1;
+  let workingTomorrow = workingDays.indexOf((currentDay + 1) % 7) > -1;
+
+  if (!show
+    || (!workingToday && !workingTomorrow)
+    || (workingToday && !workingTomorrow && now > endTime)
+    || (!workingToday && workingTomorrow && now < startTime)) {
     workArcA.style.visibility =
-    workArcB.style.visibility =
-    workArcC.style.visibility = 'hidden';
+      workArcB.style.visibility =
+      workArcC.style.visibility = 'hidden';
     return;
   }
 
-  let startAngle = util.hoursAndMinutesToFaceAngle(startHour, 0) + 180;
-  let endAngle = util.hoursAndMinutesToFaceAngle(endHour, 0) + 180;
-  let sweepAngle = endAngle - startAngle;
-  
+  let startAngle = (util.hoursAndMinutesToFaceAngle(startHour, 0) + 180) % 360;
+  let endAngle = (util.hoursAndMinutesToFaceAngle(endHour, 0) + 180) % 360;
+
+  if (startTime < now && now < endTime) {
+    if (workingToday && !workingTomorrow) {
+      startAngle = util.dateToFaceAngle(now) + 180;
+      endAngle = util.hoursAndMinutesToFaceAngle(endHour, 0) + 180;
+    }
+    if (!workingToday && workingTomorrow) {
+      startAngle = util.hoursAndMinutesToFaceAngle(startHour, 0) + 180;
+      endAngle = util.dateToFaceAngle(now) + 180;
+    }
+  }
+
+  let sweepAngle = (endAngle - startAngle + 360) % 360;
+
   workArcA.style.visibility =
-  workArcB.style.visibility =
-  workArcC.style.visibility = 'visible';
+    workArcB.style.visibility =
+    workArcC.style.visibility = 'visible';
   workArcA.startAngle =
-  workArcB.startAngle =
-  workArcC.startAngle = startAngle;
+    workArcB.startAngle =
+    workArcC.startAngle = startAngle;
   workArcA.sweepAngle =
-  workArcB.sweepAngle =
-  workArcC.sweepAngle = sweepAngle;
+    workArcB.sweepAngle =
+    workArcC.sweepAngle = sweepAngle;
 }
 
 export default setWorkArc;
