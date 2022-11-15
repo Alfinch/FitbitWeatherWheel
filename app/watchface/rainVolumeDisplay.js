70b02e35b1168ea34f52f5bf5f8691af8da37954
Alfinch <alfiewoodland@gmail.com>
2022-11-15 22:38:30 +0000

    Max rain volume shown to 1dp

--- app/watchface/rainVolumeDisplay.js
+++ app/watchface/rainVolumeDisplay.js
@@ -5,14 +5,16 @@ const rainChanceText = document.getElementById('rainChance');
 
 function setRainVolumeDisplay(show, volumes) {
 
-    let next24 = volumes.slice(0, 24);
-
-    let maxVolume = Math.max.apply(Math, next24);
-    let totalVolume = volumes.reduce((v1, v2) => v1 + v2);
-
     if (show) {
+        let next24 = volumes.slice(0, 24);
+
+        let maxVolume = Math.max.apply(Math, next24);
+        let maxVolumeDisplay = maxVolume < 10 ? Math.round(maxVolume * 10) / 10 : Math.round(maxVolume);
+
+        let totalVolume = volumes.reduce((v1, v2) => v1 + v2);
+
         rainVolumeText.style.visibility = rainChanceText.style.visibility = 'visible';
-        rainVolumeText.text = `↑${Math.round(maxVolume)}mm`;
+        rainVolumeText.text = `↑${maxVolumeDisplay}mm`;
         rainChanceText.text = `${Math.round(totalVolume)}mm`
     } else {
         rainVolumeText.style.visibility = rainChanceText.style.visibility = 'hidden';
